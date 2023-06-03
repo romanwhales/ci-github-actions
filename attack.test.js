f7db52a483821f5a58a1cc3de57d0c47163b863f
Olawale Adedeji <oadedej@LLF2T9PXGWCP.ngco.com>
2023-06-03 11:47:56 -0400

    Lint fix

--- attack.test.js
+++ attack.test.js
@@ -1,8 +1,8 @@
-const attack = require("./attack")
+const attack = require('./attack');
 
-test("should return damage sentence when called correctly",() => {
-    const result = attack("Smelly Ogre",23)
-    expect(result).toBe("Smelly Ogre dealt 23 damage!")
-    expect(result).toMatch(/^Smelly Ogre(.*)23/)
-    expect(result).toMatchInlineSnapshot(`"Smelly Ogre dealt 23 damage!"`)
-})
+test('should return damage sentence when called correctly', () => {
+  const result = attack('Smelly Ogre', 23);
+  expect(result).toBe('Smelly Ogre dealt 23 damage!');
+  expect(result).toMatch(/^Smelly Ogre(.*)23/);
+  expect(result).toMatchInlineSnapshot('"Smelly Ogre dealt 23 damage!"');
+});
