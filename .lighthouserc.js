30554e6cbed38ccd84ca956eac4062db24696b1f
Olawale Adedeji <oadedej@LLF2T9PXGWCP.ngco.com>
2023-06-10 12:08:48 -0400

    added src

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
       collect: {
         settings: {chromeFlags: '--no-sandbox'},
-        staticDistDir: "./public"
+        staticDistDir: "./src"
       },
       upload: {
         target: 'temporary-public-storage',
