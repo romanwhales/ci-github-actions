bd9a7509334506d3743bf9286c69fe85b366ed8f
Olawale Adedeji <oadedej@LLF2T9PXGWCP.ngco.com>
2023-06-10 11:45:57 -0400

    Lighthouse ci static dir fix

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,6 +2,7 @@ module.exports = {
     ci: {
       collect: {
         settings: {chromeFlags: '--no-sandbox'},
+        staticDistDir: "./dist"
       },
       upload: {
         target: 'temporary-public-storage',
